21191c0f631b4e4862c1879f48cbba537da1efde
Asif Rahman <asifrahman8@gmail.com>
2023-06-09 17:53:59 -0400

    image details should not jitter on open (due to image load)

--- src/views/Main/Browser/ImageDetails/ImageDetails.jsx
+++ src/views/Main/Browser/ImageDetails/ImageDetails.jsx
@@ -9,26 +9,49 @@ import {
   Pagination,
   ToggleButton,
   ToggleButtonGroup,
+  CircularProgress,
 } from '@mui/material'
 import { getImageUrl } from '../../../../utils/general'
 import { Close } from '@mui/icons-material'
 import { DateTime } from 'luxon'
 import './ImageDetails.css'
+import { useState } from 'react'
 
 export default function ImageDetails({ open, data, onClose, s3BucketNames }) {
+  const [imageLoaded, setImageLoaded] = useState(false)
+
+  const handleClose = () => {
+    setImageLoaded(false)
+    onClose()
+  }
+
+  const placeholderSize =
+    window.innerWidth / window.innerHeight < 1266 / 1009
+      ? (window.innerWidth * 771) / 1213
+      : Math.min((window.innerHeight * 807) / 1046, 807)
+
   return (
-    <Dialog className='image-details' maxWidth='lg' open={open} onClose={() => onClose()}>
+    <Dialog className='image-details' maxWidth='lg' open={open} onClose={handleClose}>
       {data && (
         <>
           <DialogTitle className='title-area'>
             <span>Image Details</span>
-            <IconButton onClick={() => onClose()}>
+            <IconButton onClick={handleClose}>
               <Close />
             </IconButton>
           </DialogTitle>
           <DialogContent className='dialog-content'>
             <div className='column'>
-              <img src={getImageUrl(s3BucketNames.image, data.image_id)} />
+              <img
+                src={getImageUrl(s3BucketNames.image, data.image_id)}
+                onLoad={() => setImageLoaded(true)}
+                style={{ display: imageLoaded ? 'block' : 'none', opacity: imageLoaded ? 1 : 0 }}
+              />
+              {!imageLoaded && (
+                <div className='placeholder' style={{ width: placeholderSize, height: placeholderSize }}>
+                  <CircularProgress color='warning' />
+                </div>
+              )}
             </div>
             <div className='column'>
               <div className='text-area'>
